26a06bec371ff5b703bfa5e6ca274040601dc87f
Khang Tran <khangtrann8198@gmail.com>
2022-10-01 16:39:02 +0700

    Implement product card component

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BadgeComponent } from './components/badge/badge.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { CategoryComponent } from './components/category/category.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { RatingComponent } from './components/rating/rating.component';
 
-const declarations = [BannerComponent, CategoryComponent];
+const declarations = [
+  BannerComponent,
+  CategoryComponent,
+  ProductCardComponent,
+  BadgeComponent,
+  RatingComponent
+];
 
 @NgModule({
-  imports: [CommonModule, BrowserModule, BrowserAnimationsModule, CarouselModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    HttpClientModule,
+  ],
   declarations: [...declarations],
   exports: [...declarations],
 })
