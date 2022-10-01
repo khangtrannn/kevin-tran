8e51ab191000454f5220092202f720e228dd5509
Khang Tran <khangtrann8198@gmail.com>
2022-10-02 00:04:09 +0700

    Add header from previous project

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -7,6 +7,11 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
 import { BadgeComponent } from './components/badge/badge.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { CategoryComponent } from './components/category/category.component';
+import { CartListComponent } from './components/header/components/cart-list/cart-list.component';
+import { NotifyComponent } from './components/header/components/notify/notify.component';
+import { QrCodeComponent } from './components/header/components/qr-code/qr-code.component';
+import { SearchComponent } from './components/header/components/search/search.component';
+import { HeaderComponent } from './components/header/header.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { RatingComponent } from './components/rating/rating.component';
@@ -18,6 +23,11 @@ const declarations = [
   BadgeComponent,
   RatingComponent,
   ProductListComponent,
+  NotifyComponent,
+  QrCodeComponent,
+  SearchComponent,
+  CartListComponent,
+  HeaderComponent,
 ];
 
 @NgModule({
