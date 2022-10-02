3f553cd04d8148f75f19d4eb53053c95428bc16d
Khang Tran <khangtrann8198@gmail.com>
2022-10-02 15:29:22 +0700

    Implement product attribute table

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
-export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductDetailComponent {
+  productAttributes: any[] = [
+    { attribute: 'Kho', value: '39' },
+    { attribute: 'Thương hiệu', value: 'Keychron' },
+    { attribute: 'Kiểu kết nối', value: 'Không dây' },
+    { attribute: 'Bộ sản phẩm', value: 'Có' },
+    { attribute: 'Loại bàn phím', value: 'Bàn phím cơ' },
+    { attribute: 'Hạn bảo hành', value: '12 tháng' },
+    { attribute: 'Loại bảo hành', value: 'Bảo hành nhà sản xuất' },
+    { attribute: 'Game chuyên dụng', value: 'Có' },
+    { attribute: 'Model bàn phím & chuột', value: 'Keychron K8 Pro' },
+    { attribute: 'Bàn phím tương thích', value: 'macOS/Windows/Linux' },
+    {
+      attribute: 'Kích thước (dài x rộng x cao)',
+      value: '359 x 127 x 42mm',
+    },
+    {
+      attribute: 'Tên tổ chức chịu trách nhiệm sản xuất',
+      value: 'Công Ty Cổ Phần SiliconZ',
+    },
+    {
+      attribute: 'Địa chỉ tổ chức chịu trách nhiệm sản xuất',
+      value: '72 Ngô Bệ, P.13, Q.Tân Bình, TP.HCM',
+    },
+    { attribute: 'Gửi từ', value: 'Hà Nội' },
+  ];
 }
