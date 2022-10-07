153e1683244977b1879bef42ee0557354eed7ec5
Khang Tran <khangtrann8198@gmail.com>
2022-10-07 21:00:29 +0700

    Remove header

--- src/app/share/components/header/components/notify/notify-item.ts
+++
@@ -1,6 +0,0 @@
-export interface NotifyItem {
-  name: string;
-  description: string;
-  isNew: boolean;
-  image: string;
-}
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,23 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ProductOrderComponent } from '../components/product-detail/components/product-order/product-order.component';
 import { ProductBottomPanelComponent } from './../components/product-detail/components/product-bottom-panel/product-bottom-panel.component';
 import { BadgeComponent } from './components/badge/badge.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ButtonToggleGroupComponent } from './components/button-toggle-group/button-toggle-group.component';
 import { CategoryComponent } from './components/category/category.component';
 import { DiscountBadgeComponent } from './components/discount-badge/discount-badge.component';
 import { CartListComponent } from './components/header/components/cart-list/cart-list.component';
-import { NotifyComponent } from './components/header/components/notify/notify.component';
-import { QrCodeComponent } from './components/header/components/qr-code/qr-code.component';
-import { SearchComponent } from './components/header/components/search/search.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { RatingComponent } from './components/rating/rating.component';
+
 
 const declarations = [
   BannerComponent,
@@ -26,13 +28,12 @@ const declarations = [
   BadgeComponent,
   RatingComponent,
   ProductListComponent,
-  NotifyComponent,
-  QrCodeComponent,
-  SearchComponent,
   CartListComponent,
   HeaderComponent,
   DiscountBadgeComponent,
   ProductBottomPanelComponent,
+  ProductOrderComponent,
+  ButtonToggleGroupComponent,
 ];
 
 @NgModule({
@@ -43,6 +44,8 @@ const declarations = [
     CarouselModule,
     HttpClientModule,
     RouterModule,
+    MatDialogModule,
+    MatButtonToggleModule,
   ],
   declarations: [...declarations],
   exports: [...declarations],
