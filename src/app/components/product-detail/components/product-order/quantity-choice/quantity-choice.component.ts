b6b528946b92b0490810e0979af972d146af6555
Khang Tran <khangtrann8198@gmail.com>
2022-10-15 21:20:55 +0700

    Update product order

--- src/app/components/product-detail/components/product-order/quantity-choice/quantity-choice.component.ts
+++ src/app/components/product-detail/components/product-order/quantity-choice/quantity-choice.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-quantity-choice',
   templateUrl: './quantity-choice.component.html',
-  styleUrls: ['./quantity-choice.component.scss']
+  styleUrls: ['./quantity-choice.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => QuantityChoiceComponent),
+      multi: true,
+    },
+  ]
 })
-export class QuantityChoiceComponent {
+export class QuantityChoiceComponent implements ControlValueAccessor {
   value: number = 1;
+
+  onTouched: (() => void) | undefined;
+  onChange: ((value: unknown) => void) | undefined;
+
+  increase(): void {
+    this.value += 1;
+    this.onQuantityChanged(this.value);
+  }
+
+  decrease(): void {
+    if (this.value > 1) {
+      this.value -= 1;
+      this.onQuantityChanged(this.value);
+    }
+  }
+
+  registerOnChange(onChange: (_value: unknown) => void): void {
+    this.onChange = onChange;
+  }
+
+  writeValue(value: number): void {
+    this.value = value;
+  }
+
+  registerOnTouched(onTouched: () => void): void {
+    this.onTouched = onTouched;
+  }
+
+  onQuantityChanged(value: number) {
+    this.onChange?.(value);
+  }
 }
--- src/app/components/product-detail/components/product-order/size-choice/size-choice.component.ts
+++ src/app/components/product-detail/components/product-order/size-choice/size-choice.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-size-choice',
   templateUrl: './size-choice.component.html',
-  styleUrls: ['./size-choice.component.scss']
+  styleUrls: ['./size-choice.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SizeChoiceComponent),
+      multi: true,
+    },
+  ],
 })
-export class SizeChoiceComponent {
+export class SizeChoiceComponent implements ControlValueAccessor {
   options: string[] = ['S', 'M', 'L', 'XL'];
+
+  value: string | undefined;
+  onTouched: (() => void) | undefined;
+  onChange: ((fieldValue: unknown) => void) | undefined;
+
+  registerOnChange(onChange: (_fieldValue: unknown) => void): void {
+    this.onChange = onChange;
+  }
+
+  writeValue(fieldValue: string): void {
+    this.value = fieldValue;
+  }
+
+  registerOnTouched(onTouched: () => void): void {
+    this.onTouched = onTouched;
+  }
+
+  onSizeChanged(value: string) {
+    this.onChange?.(value);
+  }
 }
