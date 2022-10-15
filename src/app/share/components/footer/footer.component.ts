aefc54d957ec76b2ce74b09aee01d6ce330d60be
Khang Tran <khangtrann8198@gmail.com>
2022-10-15 17:51:04 +0700

    Update footer

--- src/app/share/components/footer/footer.component.ts
+++ src/app/share/components/footer/footer.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { IS_MOBILE } from 'src/app/models/constants';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  constructor (@Inject(IS_MOBILE) public isMobile: boolean) {}
+}
