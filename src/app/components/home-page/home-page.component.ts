58f769f470440542dd4e576afc1775750b8739ed
Khang Tran <khangtrann8198@gmail.com>
2022-10-22 22:01:12 +0700

    Only show header menu on home page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,26 @@
-import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { IS_MOBILE } from 'src/app/models/constants';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-  constructor(@Inject(IS_MOBILE) public isMobile: boolean) {}
+export class HomePageComponent implements OnInit, OnDestroy {
+  readonly HOST_CLASS = 'home-page';
+
+  constructor(
+    @Inject(IS_MOBILE) public isMobile: boolean,
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    this.renderer.addClass(this.document.body, this.HOST_CLASS);
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(this.document.body, this.HOST_CLASS);
+  }
 }
